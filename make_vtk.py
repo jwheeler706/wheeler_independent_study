6c3cf0758c07d650a2e3755ef38618af50998392
Jeff Wheeler <jwheeler706@gmail.com>
2018-05-06 06:36:19 -0400

    updated vtk

--- make_vtk.py
+++ make_vtk.py
@@ -3,15 +3,15 @@
 import vtk
 import glob, os, sys
 
-def make_vtk_files(path):
-    for i,file in enumerate(glob.glob(path+"*")):
+def make_vtk_files(in_path, out_path):
+    for i,file in enumerate(glob.glob(in_path+"*")):
 
         # These lines get the names of the obj file and makes the output name
         # They might need to be adjusted based on file naming scheme
         # Output name doesn't matter except each file should end with a number
         e = file.split('/')[0]
         n_frame = int(file.split('DD')[1].split('_')[0])-50
-        output_name = path+'/vtk/'+e+'_density_'+str(n_frame)+'.vtk'
+        output_name = out_path+e+'_density_'+str(n_frame)+'.vtk'
 
         reader = vtk.vtkOBJReader()
         reader.SetFileName(file)
@@ -24,5 +24,7 @@ def make_vtk_files(path):
         writer.Update()
         writer.Write()
 
-path = sys.argv[1]
-make_vtk_files(path)
+in_path = sys.argv[1]
+out_path = sys.argv[2]
+
+make_vtk_files(in_path, out_path)
