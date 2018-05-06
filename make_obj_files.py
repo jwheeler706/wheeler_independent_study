e0ac063b8f0d8f316df64d81e7b32e9a2e82a05b
Jeff Wheeler <jwheeler706@gmail.com>
2018-05-06 02:24:50 -0400

    updated sumbit and obj

--- make_obj_files.py
+++ make_obj_files.py
@@ -1,11 +1,11 @@
 import yt
 import sys
 
-def make_plots(folder,color,idk_what_this_number_means):
+def make_plots(folder,color,density):
     ds = yt.load('/mnt/research/galaxies-REU/sims/isolated-galaxies/MW_1638kpcBox_800pcCGM_200pcDisk_lowres/'+folder+'/'+folder)
     
-    filename = './e'+str(idk_what_this_number_means)+'/'+folder+'_e'+str(idk_what_this_number_means)+'_'+color
-    rho = [1*10**-int(idk_what_this_number_means)]#,1e-25,1e-26,1e-27]
+    filename = './e'+str(density)+'/'+folder+'_e'+str(density)+'_'+color
+    rho = [1*10**-int(density)]#,1e-25,1e-26,1e-27]
     print(rho)
     trans = [1.0]#,0.50,0.25]
     sphere = ds.sphere(ds.domain_center, (300, 'kpc'))
@@ -19,9 +19,9 @@ def make_plots(folder,color,idk_what_this_number_means):
 folders = ['DD'+'{0:0>4}'.format(i) for i in range(2015,2500)]
 #folders = ['DD0914']
 
-weird_number = sys.argv[1]
+density = sys.argv[1]
 
 for folder in folders:
-    make_plots(folder,'density',weird_number)
+    make_plots(folder,'density',density)
     #make_plots(folder,'metallicity')
     
